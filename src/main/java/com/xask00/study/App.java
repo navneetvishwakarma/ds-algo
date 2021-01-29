401930f83f3121b1ee6a8807d1a33eff630c0f3c
Navneet Vishwakarma <navneet.nifft@gmail.com>
2021-01-29 12:13:09 +0530

    initial maven project commit

---
+++ src/main/java/com/xask00/study/App.java
@@ -0,0 +1,13 @@
+package com.xask00.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to DS and Algorithm implementation!" );
+    }
+}
