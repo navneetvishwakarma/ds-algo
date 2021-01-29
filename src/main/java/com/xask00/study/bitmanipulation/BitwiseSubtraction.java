49803d1ea5b822e8de85405034aaba785bd296b0
Navneet Vishwakarma <navneet.nifft@gmail.com>
2021-01-29 12:16:41 +0530

    Bitwise addition and subtraction

---
+++ src/main/java/com/xask00/study/bitmanipulation/BitwiseSubtraction.java
@@ -0,0 +1,18 @@
+package com.xask00.study.bitmanipulation;
+
+public class BitwiseSubtraction {
+    public static void main(String[] args) {
+        int a = 90;
+        int b = 180;
+        System.out.println("Diff: " + subtract(a, b));
+    }
+
+    public static int subtract(int a, int b) {
+        while(b!=0) {
+            int borrow = (~a) & b;
+            a = a^b;
+            b = borrow << 1;
+        }
+        return a;
+    }
+}
---
+++ src/main/java/com/xask00/study/bitmanipulation/BitwiseSum.java
@@ -0,0 +1,21 @@
+package com.xask00.study.bitmanipulation;
+
+/**
+ * Problem: Add two numbers without using arithmetic operators (by adding two numbers)
+ */
+public class BitwiseSum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 568;
+        System.out.println("Sum: " + sum(a, b));
+    }
+
+    public static int sum(int a, int b) {
+        while(b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}
